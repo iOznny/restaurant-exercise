feb15e3602646c8cb4d370c48f6ee277ba9573d8
iOznny <fo170014@gmail.com>
2020-12-17 22:29:33 -0600

    Insert correcto

--- create.php
+++ create.php
@@ -1,5 +1,6 @@
 <?php
 
+  // Variables de View.
   $m_name = $_POST['name'];
   $m_description = $_POST['description'];
   $m_price = $_POST['price'];
@@ -10,47 +11,21 @@
 	$password = "Hola2020@@";
 	$database = "schoolst_testing";
 	
-	// Creación de la conexión a la Base de Datos con mysql_connect()
+	// Creación de la conexión a la Base de Datos con mysql_connect().
   try {
     	$conexion = new PDO('mysql:host='.$server.';dbname='.$database, $username, $password);
     	$conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     	$conexion->exec("SET CHARACTER SET utf8");  
-      echo "Conexión satisfactoria.";
-      
-      // Crear menu.
-      //insertarMenu();
+      echo "Conexión satisfactoria." . '<br>';
 
-  } catch (PDOException $e) {
-    	echo "Error: " . $e->getMessage();  
-    	echo "Conexión sin éxito.";   
-	}
-     
-  
-  function insertarMenu() {
-    // Ingresar Menu.
-    try { 
-      // Consulta SQL
-      $sql = "INSERT INTO menu VALUES (null, '$m_name', '$m_description', '$m_price');";
-      $resultado = $conexion->prepare($sql);
+      // Crear menu.
+      $query = "INSERT INTO menus VALUES (null, '$m_name', '$m_description', '$m_price');";
+      $resultado = $conexion->prepare($query);
       $resultado->execute();
       echo "Menu registrado exitosamente.";
-      
-    } catch(PDOException $e) {
-      echo "Error: " . $e->getMessage();  
-      echo "Ocurrio un error al registrar el menu, verifique e intente más tarde.";
-    }
-  }
-
-  
-  /* Pedimos la información remota */
-	/* $sql = 'SELECT * FROM menus';
-  foreach ($conexion->query($sql) as $row) {
-    print $row['id'] . ' - ' . $row['name'] . '<br>';
-  } */
-
-
-  
-
-
-
+  } catch (PDOException $e) {
+    	echo "Error: " . $e->getMessage();  
+    	echo "Conexión sin éxito." . '<br>';   
+	}
+     
 ?>
